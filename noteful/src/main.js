bfd831c1f4c3557d482e6f68c985890cf4249535
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-19 09:33:00 -0700

    Main function added in

---
+++ noteful/src/main.js
@@ -0,0 +1,15 @@
+import React from "./React";
+import "./main.css";
+
+// class Main extends Component {
+
+const Main = () => {
+  return;
+  <div>
+    <header className="mainHeader">
+      <link to="/">Noteful</link>
+    </header>
+  </div>;
+};
+
+export default Main;
