a91ee3b30497a127ea5368a16ad3ea6334900c0c
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-19 13:22:58 -0700

    swapping drivers

--- noteful/src/App.js
+++ noteful/src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import Main from './main';
-
+import React, { Component } from "react";
+import Main from "./main";
+import { Route } from "react-router-dom";
 
 class App extends Component {
-    render () {
-        return (
-            <Main />
-        )
-    }
+  render() {
+    return <Main />;
+  }
 }
 
 export default App;
+
+// basic / route
+// /folder
+// /folder.id
+// /note
+// /note.id
+
+//route for every url
+//route for
--- noteful/src/folder.js
+++ noteful/src/folder.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React from "react";
 
 const folder = () => {
-    return (
-        <div>
-            <header className="mainHeader">
-                {/* <link to="/">Noteful</link> */}
-            </header>
+  return (
+    <div>
+      <header className="mainHeader">
+        {/* <link to="/">Noteful</link> */}
+      </header>
 
-            <section className="sideBar">
-            <ul>
-                <li>Folder 1</li>
-                <li>Folder 2</li>
-                <li>Folder 3</li>
-            </ul>
-            <button>Add folder</button>
-            </section>
+      <section className="sideBar">
+        <ul>
+          <li>Folder 1</li>
+          <li>Folder 2</li>
+          <li>Folder 3</li>
+        </ul>
+        <button>Add folder</button>
+      </section>
 
-            <section className="mainSection">
-            <ul>
-                <li><div>
-                <h2>Note 1</h2>
-                <p>Date modified info</p>
-                <button>Delete Note</button>
-                </div></li>
+      <section className="mainSection">
+        <ul>
+          <li>
+            <div>
+              <h2>Note 1</h2>
+              <p>Date modified info</p>
+              <button>Delete Note</button>
+            </div>
+          </li>
 
-                <li><div>
-                <h2>Note 2</h2>
-                <p>Date modified info</p>
-                <button>Delete Note</button>
-                </div></li>
-            </ul>
+          <li>
+            <div>
+              <h2>Note 2</h2>
+              <p>Date modified info</p>
+              <button>Delete Note</button>
+            </div>
+          </li>
+        </ul>
 
-            <button>Add note</button>
-            </section>
-
-        </div>
-    )
-}
+        <button>Add note</button>
+      </section>
+    </div>
+  );
+};
 
 export default folder;
+
+{
+  /* <Route
+path="/note/:noteId"
+render={routeProps => {
+    const {noteId} = routeProps.match.params;
+    const note = findNote(notes, noteId) || {};
+    const folder = findFolder(folders, note.folderId);
+    return <NotePageNav {...routeProps} folder={folder} />;
+}}
+/> */
+}
